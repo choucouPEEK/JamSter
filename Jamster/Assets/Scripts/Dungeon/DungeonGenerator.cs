e9c12b39253a5c63aa583bd4514ebf64504fb74d
t.fournel <t.fournel@student.isartdigital.com>
2024-10-25 18:28:13 +0200

    DungeonGeneration Add (RoomPlacementOnGrid)

--- Jamster/Assets/Scripts/Dungeon/DungeonGenerator.cs
+++ Jamster/Assets/Scripts/Dungeon/DungeonGenerator.cs
@@ -1,143 +1,103 @@
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    [SerializeField] private DungeonRoom startRoom;
+    [SerializeField] public List<DungeonRoom> roomPrefabs = new List<DungeonRoom>();
+    public List<Vector3> allRoomPosition = new List<Vector3>();
 
-    [SerializeField] public static List<DungeonRoom> roomPrefabs = new List<DungeonRoom>();
-    [SerializeField] public static List<DungeonRoom> endRoomPrefab = new List<DungeonRoom>();
+    // Grid Variables
+    [SerializeField] private Vector2 gridSize = new Vector2();
+    [SerializeField] private Vector2 roomSize = new Vector2();
 
-    public static List<Vector3> allRoomPosition = new List<Vector3>();
-    void Start()
+    [SerializeField] private int roomLeft; // Total rooms to generate Aroud
+    private List<List<DungeonRoom>> roomList;
+    private Vector2 startingPointPosition;
+
+    public void Start()
     {
-        //RecursiveRoomGeneration(startRoom, 8);
+        ChooseStartingPoint();
+        CreateGrid();
+        RecursiveRoomCreation(roomLeft, startingPointPosition);
+        InstantiateGrid();
     }
 
-    private void RecursiveRoomGeneration(DungeonRoom lPreviousRoom, int lMaxRoomLeft) //Create Rooms with Recursivity, maxRoomLeft say how much room in any way can spawn
+    // Choose a random starting point
+    private void ChooseStartingPoint()
     {
-        if (lMaxRoomLeft == 0)
+        startingPointPosition = new Vector2(Mathf.Floor(Random.Range(0, gridSize.x)), Mathf.Floor(Random.Range(0, gridSize.y)));
+    }
+
+    // Create the grid with room prefabs
+    private void CreateGrid()
+    {
+        roomList = new List<List<DungeonRoom>>();
+
+        for (int x = 0; x < gridSize.x; x++)
         {
-            DungeonRoom room;
-            /*if (lPreviousRoom.isRoomOpenUp)
+            roomList.Add(new List<DungeonRoom>());
+
+            for (int y = 0; y < gridSize.y; y++)
             {
-                room = FindEndRoom(lPreviousRoom, lMaxRoomLeft, "up");
-                PlaceRoom(lPreviousRoom, room, "up");
-            }
-            if (lPreviousRoom.isRoomOpenDown)
-            {
-                room = FindEndRoom(lPreviousRoom, lMaxRoomLeft, "down");
-                PlaceRoom(lPreviousRoom, room, "down");
-            }
-            if (lPreviousRoom.isRoomOpenLeft)
-            {
-                room = FindEndRoom(lPreviousRoom, lMaxRoomLeft, "left");
-                PlaceRoom(lPreviousRoom, room, "left");
-            }
-            if (lPreviousRoom.isRoomOpenRight)
-            {
-                room = FindEndRoom(lPreviousRoom, lMaxRoomLeft, "right");
-                PlaceRoom(lPreviousRoom, room, "right");
-            }*/
-        }
-        else if (lMaxRoomLeft > 0)
-        {
-            DungeonRoom room;
-            if (lPreviousRoom.isRoomOpenUp)
-            {
-                room = FindRandomRoom(lPreviousRoom, lMaxRoomLeft, "up");
-                PlaceRoom(lPreviousRoom, room, "up");
-                allRoomPosition.Add(room.transform.position);
-                RecursiveRoomGeneration(room, lMaxRoomLeft - 1);
-            }
-            if (lPreviousRoom.isRoomOpenDown)
-            {
-                room = FindRandomRoom(lPreviousRoom, lMaxRoomLeft, "down");
-                PlaceRoom(lPreviousRoom, room, "down");
-                allRoomPosition.Add(room.transform.position);
-                RecursiveRoomGeneration(room, lMaxRoomLeft - 1);
-            }
-            if (lPreviousRoom.isRoomOpenLeft)
-            {
-                room = FindRandomRoom(lPreviousRoom, lMaxRoomLeft, "left");
-                PlaceRoom(lPreviousRoom, room, "left");
-                allRoomPosition.Add(room.transform.position);
-                RecursiveRoomGeneration(room, lMaxRoomLeft - 1);
-            }
-            if (lPreviousRoom.isRoomOpenRight)
-            {
-                room = FindRandomRoom(lPreviousRoom, lMaxRoomLeft, "right");
-                PlaceRoom(lPreviousRoom, room, "right");
-                allRoomPosition.Add(room.transform.position);
-                RecursiveRoomGeneration(room, lMaxRoomLeft - 1);
+                if (x == (int)startingPointPosition.x && y == (int)startingPointPosition.y)
+                {
+                    roomList[x].Add(roomPrefabs[0]); // Place the starting room
+                }
+                else
+                {
+                    roomList[x].Add(null); // Empty cell
+                }
             }
         }
     }
 
-    private DungeonRoom FindRandomRoom(DungeonRoom lPreviousRoom, int lMaxRoomLeft, string lDirection)
+    // Recursive function to create rooms
+    private void RecursiveRoomCreation(int roomLeft, Vector2 currentPosition)
     {
-        List<DungeonRoom> lPossibleRoom = new List<DungeonRoom>();
-        DungeonRoom lSelectedRoom;
-        Vector3 lPreviousPosition = lPreviousRoom.transform.position;
-        for (int room = 0; room < roomPrefabs.Count; room++)
+        // Base case: no more rooms to place
+        if (roomLeft <= 0)
+            return;
+
+        // Possible directions (right, left, up, down)
+        Vector2[] directions = { Vector2.right, Vector2.left, Vector2.up, Vector2.down };
+
+        // Try placing rooms in the available directions
+        foreach (Vector2 direction in directions)
         {
-            if (lDirection == "down" && roomPrefabs[room].isRoomOpenUp && IsPlaceFree(lPreviousPosition + new Vector3(0, 0, 10)))
-                lPossibleRoom.Add(roomPrefabs[room]);
+            Vector2 nextPosition = currentPosition + direction;
 
-            if (lDirection == "up" && roomPrefabs[room].isRoomOpenDown && IsPlaceFree(lPreviousPosition + new Vector3(0, 0, -10)))
-                lPossibleRoom.Add(roomPrefabs[room]);
+            // Check if the next position is valid
+            if (IsPositionInGrid(nextPosition) && roomList[Mathf.FloorToInt(nextPosition.x)][Mathf.FloorToInt(nextPosition.y)] == null)
+            {
+                // Place the room
+                roomList[Mathf.FloorToInt(nextPosition.x)][Mathf.FloorToInt(nextPosition.y)] = roomPrefabs[0];
+                roomLeft--; // Decrement room count
 
-            if (lDirection == "left" && roomPrefabs[room].isRoomOpenRight && IsPlaceFree(lPreviousPosition + new Vector3(10, 0, 0)))
-                lPossibleRoom.Add(roomPrefabs[room]);
-
-            if (lDirection == "right" && roomPrefabs[room].isRoomOpenLeft&& IsPlaceFree(lPreviousPosition + new Vector3(-10, 0, 0)))
-                lPossibleRoom.Add(roomPrefabs[room]);
+                // Recursively try to create more rooms from the new position
+                RecursiveRoomCreation(roomLeft-1, nextPosition);
+            }
         }
-        lSelectedRoom = lPossibleRoom[Random.Range(0, lPossibleRoom.Count-1)];
-        return lSelectedRoom; 
-    }
-    /*private DungeonRoom FindEndRoom(DungeonRoom lPreviousRoom, int lMaxRoomLeft, string lDirection)
-    {
-        for (int room = 0; room < existingEndRoom.Count; room++)
-        {
-            if (lDirection == "down" && existingEndRoom[room].isRoomOpenUp)
-                return existingEndRoom[room];
-
-            if (lDirection == "up" && existingEndRoom[room].isRoomOpenDown)
-                return existingEndRoom[room];
-
-            if (lDirection == "left" && existingEndRoom[room].isRoomOpenRight)
-                return existingEndRoom[room];
-
-            if (lDirection == "right" && existingEndRoom[room].isRoomOpenLeft)
-                return existingEndRoom[room];
-        }
-        return null;
-    }*/
-
-    private void PlaceRoom(DungeonRoom lPreviousRoom, DungeonRoom actualRoom, string lDirection)
-    {
-        Instantiate(actualRoom);
-        Debug.Log(actualRoom.name);
-        if(lDirection == "down")
-            actualRoom.transform.position = lPreviousRoom.transform.position+new Vector3(0,0,-10);
-        else if (lDirection == "up")
-            actualRoom.transform.position = lPreviousRoom.transform.position + new Vector3(0, 0, 10);
-        else if (lDirection == "left")
-            actualRoom.transform.position = lPreviousRoom.transform.position + new Vector3(-10, 0, 0);
-        else if (lDirection == "right")
-            actualRoom.transform.position = lPreviousRoom.transform.position + new Vector3(10, 0, 0);
     }
 
-    private bool IsPlaceFree(Vector3 lPosition)
+    // Check if the position is within bounds
+    private bool IsPositionInGrid(Vector2 position)
     {
-        for (int i = 0; i < allRoomPosition.Count; i++)
+        return position.x >= 0 && position.x < gridSize.x && position.y >= 0 && position.y < gridSize.y;
+    }
+    
+    // Instantiate the rooms in the scene
+    private void InstantiateGrid()
+    {
+        for (int x = 0; x < gridSize.x; x++)
         {
-            if (allRoomPosition[i] == lPosition) return false;
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                if (roomList[x][y] != null)
+                {
+                    DungeonRoom instantiatedRoom = Instantiate(roomList[x][y]);
+                    instantiatedRoom.transform.position = new Vector3(x * roomSize.x, 0, y * roomSize.y);
+                }
+            }
         }
-        return true;
     }
 }
