bcb43922990bab104b69d0e35e29b1380c3e517f
MegaLoGiK9 <mael398x@gmail.com>
2024-10-25 02:14:16 +0200

    dev - Player Movements + detect Walls
    player now moves, has gravity and detect walls with 4 raycast and "Walls" tag on wall object

--- Jamster/Assets/Scripts/Entities/Player/PlayerMovement.cs
+++ Jamster/Assets/Scripts/Entities/Player/PlayerMovement.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] private float _PlayerWidth = 1; //used for raycast to the walls
+    [SerializeField] private string _TagWalls;
+
+    //Movements
     [SerializeField] private float _MaxSpeed = 20;
     [SerializeField] private string _HorizontalAxis = "Horizontal";
     [SerializeField] private string _VerticalAxis = "Vertical";
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckForWalls();
         Movements();
+        
     }
 
     private void Movements()
@@ -25,5 +35,45 @@ public class PlayerMovement : MonoBehaviour
         Vector3 lInput = new Vector3(Input.GetAxis(_HorizontalAxis), 0, Input.GetAxis(_VerticalAxis));
         lInput = Vector3.ClampMagnitude(lInput, 1);
         transform.position += transform.TransformDirection(lInput) * (_MaxSpeed * Time.deltaTime);
+    }
+
+    private void CheckForWalls()
+    {
+        Vector3 myPosition = transform.position;
+        Vector3 rayDirection = transform.forward;
+        RaycastHit hitInfo;
+        //forward:
+        if (Physics.Raycast(myPosition, rayDirection, out hitInfo, _PlayerWidth))
+        {
+            if (hitInfo.collider.gameObject.tag == _TagWalls.ToString())
+            {
+                transform.position -= transform.forward * (_MaxSpeed * Time.deltaTime);
+            }
+        }
+        //backward
+        if (Physics.Raycast(myPosition, -rayDirection, out hitInfo, _PlayerWidth))
+        {
+            if (hitInfo.collider.gameObject.tag == _TagWalls.ToString())
+            {
+                transform.position += transform.forward * (_MaxSpeed * Time.deltaTime);
+            }
+        }
+        rayDirection = transform.right;
+        //right
+        if (Physics.Raycast(myPosition, rayDirection, out hitInfo, _PlayerWidth))
+        {
+            if (hitInfo.collider.gameObject.tag == _TagWalls.ToString())
+            {
+                transform.position -= transform.right * (_MaxSpeed * Time.deltaTime);
+            }
+        }
+        //left
+        if (Physics.Raycast(myPosition, -rayDirection, out hitInfo, _PlayerWidth))
+        {
+            if (hitInfo.collider.gameObject.tag == _TagWalls.ToString())
+            {
+                transform.position += transform.right * (_MaxSpeed * Time.deltaTime);
+            }
+        }
     }
 }
