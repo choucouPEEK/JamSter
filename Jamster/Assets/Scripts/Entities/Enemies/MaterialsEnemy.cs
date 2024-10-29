aea57edf6e9d814afe306180e7d62882316fa6b8
Yanis Kohlhepp <ynoam1kohlhepp@gmail.com>
2024-10-29 16:24:20 +0100

    Changer material sur les enemies fini

---
+++ Jamster/Assets/Scripts/Entities/Enemies/MaterialsEnemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialsEnemy : MonoBehaviour 
+{
+    [SerializeField] private Material _BaseMaterial;
+    [SerializeField] private Material _DarkMaterial;
+    [SerializeField] public float reducingSpeed;
+    private MeshRenderer _Renderer;
+    public bool isInLight = true;
+    private Color _Color;
+    private float _ReducingSpeed = .001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Renderer = GetComponent<MeshRenderer>();
+        _Color = _DarkMaterial.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isInLight)
+        {
+            ChangeAlpha();
+        }
+    }
+    public void InLight()
+    {
+        if (isInLight)
+        {
+            _Renderer.material = _BaseMaterial;
+        }
+        
+    }
+    private void ChangeAlpha()
+    {
+        _Color.a = 1;
+        _DarkMaterial.color = _Color;
+        _Renderer.material = _DarkMaterial;
+        _Color.a -= reducingSpeed * Time.deltaTime;
+        if (_Color.a <= 0)
+        {
+            isInLight = true;
+        }
+    }
+}
